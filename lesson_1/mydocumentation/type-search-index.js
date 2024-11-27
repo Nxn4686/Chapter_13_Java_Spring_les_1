de2b4b8f3178bc5ac47de2cf3c8bbe81b5a3c167
Nikita Lunev <102972594+Nxn4686@users.noreply.github.com>
2024-11-27 11:13:22 +0300

    Add files via upload

---
+++ lesson_1/mydocumentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ru.gb.lunev","l":"Decorator"},{"p":"ru.gb.lunev","l":"Main"},{"p":"ru.gb.lunev","l":"OtherClass"}];updateSearchResults();
